5e99f37377e4cbf8550bf4646809a4821465a514
Haydn Fewkes <haydnsfewkes@outlook.com>
2024-08-14 13:29:50 +0100

    Milestone 1

---
+++ README.mk
@@ -0,0 +1,14 @@
+AiCore PowerBI Data Analysis Project:
+
+Milestone 1:
+- Imported the tables used for the project which included:
+	- Azure SQL database
+	- CSV file
+	- Azure blob storage
+	- A folder of excel files, which were merged into one file
+- Transformed the data including:
+	- Removing unused columns
+	- Combining columns
+	- Splitting columns
+	- Ensuring all columns and table names were the same naming convention
+	- Removing null and blank values
